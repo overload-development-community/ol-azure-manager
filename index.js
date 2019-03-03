7b407481c10b220542c725cacab1ca333c6bb241
Ronald M. Clifford <roncli@roncli.com>
2019-03-02 23:35:40 -0800

    Version 1.0.0.

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const browser = require("./browser"),
         process.stdout.write("\x1b]2;Overload Azure Manager\x1b\x5c");
     }
 
-    setInterval(async () => {
+    setInterval(() => {
         browser.check();
     }, 60000);
 
