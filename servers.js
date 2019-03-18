b80f99576aef0e7bf8b31927fe9c4ecebb485178
Ronald M. Clifford <roncli@roncli.com>
2019-03-17 19:52:05 -0700

    Add IP address to status update.

--- servers.js
+++ servers.js
@@ -69,7 +69,7 @@ class Servers {
                 browser.removeAllListeners(server.ipAddress);
             }, 3600000);
 
-            await Discord.queue(`${region} lobby status: ${data.map} ${data.mode}, ${data.numPlayers}/${data.maxNumPlayers} players`, channel);
+            await Discord.queue(`${region} lobby status: ${data.map} ${data.mode}, ${data.numPlayers}/${data.maxNumPlayers} players\nJoin at **${server.ipAddress}**`, channel);
         });
     }
 }
